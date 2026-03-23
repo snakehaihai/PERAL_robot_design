c34173778066d87d4672feaff00eeb38dcde133c
JasonY85 <jasonyee2009@gmail.com>
2026-03-23 15:13:27 +0800

    Rearrange

--- microros_esp32/lib/microros/microros.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-void setup_microros();
-void microros_loop();
-void publish_wheel_speeds(float left_speed, float right_speed);
-
-extern unsigned long last_cmd_vel_time;
-extern float linear_vel;
-extern float angular_vel;
--- microros_esp32/lib/motor_control/motor_control.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-//Function declaration
-void setup_motors();
-void set_motor_speed(int pwm_pin, int in1_pin, int in2_pin, int pwm);
